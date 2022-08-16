7cabd5afcf44f9599cfbb178ebdab71305438740
ziuge <xwcjwwd@ewhain.net>
2022-08-16 09:44:12 +0900

    Initial Commit

---
+++ SeSAC7Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC7Diary
+//
+//  Created by CHOI on 2022/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
